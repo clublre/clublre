fad97e5eb154cb64fc2908e25a00bfe5c318151f
Ezequiel Rivas <69357286+CheizeX@users.noreply.github.com>
2024-08-20 21:04:14 -0300

    feat: first commit

---
+++ config/site.ts
@@ -0,0 +1,70 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "CLUB L.R.E",
+  description: "Club Los Rosarinos Estudiantil",
+  navItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "El Club",
+      href: "/about",
+    },
+    // {
+    //   label: "Blog",
+    //   href: "/blog",
+    // },
+  ],
+  navMenuItems: [
+    // {
+    //   label: "Profile",
+    //   href: "/profile",
+    // },
+    // {
+    //   label: "Dashboard",
+    //   href: "/dashboard",
+    // },
+    // {
+    //   label: "Projects",
+    //   href: "/projects",
+    // },
+    // {
+    //   label: "Team",
+    //   href: "/team",
+    // },
+    // {
+    //   label: "Calendar",
+    //   href: "/calendar",
+    // },
+    // {
+    //   label: "Settings",
+    //   href: "/settings",
+    // },
+    // {
+    //   label: "Help & Feedback",
+    //   href: "/help-feedback",
+    // },
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "El Club",
+      href: "/about",
+    },
+    // {
+    //   label: "Logout",
+    //   href: "/logout",
+    // },
+  ],
+  links: {
+    instagram: "https://www.instagram.com/clubestudiantilrosario/",
+    // github: "https://github.com/nextui-org/nextui",
+    // twitter: "https://twitter.com/getnextui",
+    // docs: "https://nextui.org",
+    // discord: "https://discord.gg/9b6yyZKmH4",
+    // sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import {nextui} from '@nextui-org/theme'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        "pop-blob": {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.2)" },
+          "66%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+        colors: {
+          filter: {
+            "blur-20": "blur(20px)",
+            "blur-25": "blur(25px)",
+          },
+        },
+        animation: {
+          "pop-blob": "pop-blob 5s infinite",
+        },
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
